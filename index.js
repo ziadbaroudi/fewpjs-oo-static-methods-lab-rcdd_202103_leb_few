5042f6449502dd0861b85f58f2552c0e0d97a61e
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-07 02:23:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
   //add static methods here
+  static capitalize (s){
+    return s.charAt(0).toUpperCase() + s.slice(1);
+    
+  }
+  static sanitize (s){
+    return s.replace(/[^a-z0-9+ /'-]+/gi, '');
+  }
+
+  static titleize (s){
+    return s.replace(/\b\w[^aanbutandforatbyfromtheof]+/gi, function(l){ return l.charAt(0).toUpperCase() + l.slice(1) })
+  }
 }
+
+
+

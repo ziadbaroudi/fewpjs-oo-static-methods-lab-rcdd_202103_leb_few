9c7ee646e9f82d117714ca213e30caebb708eb21
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-08 12:59:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,19 +11,12 @@ class Formatter {
   static titleize (s){
     let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
     let sa = s.split(" ")
-    for(let i=0 ; i<arr.length ; i++){
+    let result = sa.map(el =>{ 
+      if (arr.find(e=> e === el  
+      })
       
-      for (let j = 0; j<sa.length;j++){
-        if(sa[j] !== arr[i]){
-          this.capitalize(sa[j])
-          console.log(arr[i])
-          // console.log(sa[j])
-        }
-      }
-    }
-    return sa.join("")
   }
-}
+
   //   return s.replace(/\b[^of,the]+/gi, function(l){ 
   //     console.log(l) 
   //     return l.charAt(0).toUpperCase() + l.slice(1)  })

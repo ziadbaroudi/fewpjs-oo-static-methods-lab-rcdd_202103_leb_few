ef7797d0e0f8fb101af45607cf092219a09c65cf
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-08 13:03:31 +0000

    Done.

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ class Formatter {
   }
 
   static titleize (s){
-    return s.replace(/\b\w[^aanbutandforatbyfromtheof]+/gi, function(l){ return l.charAt(0).toUpperCase() + l.slice(1) })
-  }
-}
+    let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let sa = s.split(" ")
+    let result = sa.map((el,i) =>{ 
+      if(i===0){
+        return this.capitalize(el)
+      }
+      if (arr.find(e=> e === el)){
+        return el
+      }
+      return this.capitalize(el)
+       
+      })
+      
+      
+      
+      return result.join(" ")
+      
+  }}
 
-
+  //   return s.replace(/\b[^of,the]+/gi, function(l){ 
+  //     console.log(l) 
+  //     return l.charAt(0).toUpperCase() + l.slice(1)  })
+  // }
 
